4a37ee45a202e8b436963c95ec44998c8621c6ae
Daniel Palacios <daniel80380@gmail.com>
2022-11-08 13:00:25 -0300

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.qfGtipKT}>
+      </View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  qfGtipKT: {
+    backgroundColor: "#a03535",
+    padding: 10,
+    position: "relative",
+    height: "100%"
+  }
+});
